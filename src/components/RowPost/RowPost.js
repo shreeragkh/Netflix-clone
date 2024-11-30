c885fb1b6072f8a493db68ece713b16017fa9222
shreeragkh <shreeragkh@gmail.com>
2024-11-30 16:55:08 +0530

    added youtube trailer playing when mouse onclick

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -2,9 +2,11 @@ import React, {useEffect, useState} from 'react'
 import {baseUrl,ACCESS_TOKEN,imageUrl, genreUrl} from '../../utils/api'
 import '../RowPost/RowPost.css'
 import axios from 'axios'
+import YouTube from 'react-youtube'
 
 function RowPost(props) {
   const [movies, setMovie] = useState([])
+  const [movieId, setMovieid] = useState('')
    let URL = props.genreId ? genreUrl(props.genreId) : `${baseUrl}/trending/all/day?language=en-US`
 useEffect(() => {
     axios
@@ -23,13 +25,27 @@ useEffect(() => {
       });
   }, [URL]); 
 
-  const youtubeVideo={
+  const youtubeTrailer={
     height:390,
-    width:'100%'
+    width:'100%',
+    playerVars:{
+      autoplay:1
+    }
   };
 
   const movieTrailer = (id) => {
-    console.log(id)
+    // console.log(id)
+    axios.get(`${baseUrl}/movie/${id}/videos?language=en-US` , {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        Accept: 'application/json',
+      },}).then(response=>{
+        if (response.data.results.length!==0) {
+          setMovieid(response.data.results[0])
+        } else {
+          alert('No youtube trailer found')
+        }
+      })
   }
   
   return (
@@ -40,6 +56,13 @@ useEffect(() => {
          <img onClick={()=>movieTrailer(obj.id)} className={props.small ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="posters" />
         )}
       </div>
+      {
+        movieId && movieId.key &&(
+          <YouTube
+          videoId={movieId.key}
+          opts={youtubeTrailer}/>
+        )
+      }
     </div>
   )
 }
